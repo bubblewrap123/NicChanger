8f72fbf47280d8489135cdb5d8dd618db71512a8
fredrik444 <b1934249@rmqkr.net>
2016-02-16 17:29:24 +0100

    Added change networkadapter logi

--- powerCLI/Change-NIC.ps1
+++ powerCLI/Change-NIC.ps1
@@ -1,1 +1,20 @@
-get-vm $VM|get-networkadapter|set-networkadapter -type vmxnet3
+
+$FilePath = "c:\tools\Network_Interface1.csv"
+
+
+import-CSV $FilePath | ForEach-Object {
+	$VM = $_.VM
+	$VMPowerstate = $_.Powerstate
+	
+	if ($VMPowerstate -eq "PoweredOff") {
+		get-vm $VM|get-networkadapter|set-networkadapter -type vmxnet3
+		write-host "Nic changed on:" $VM
+		Start-VM $VM #-RunAsync
+		write-host $VM "started"
+	}
+	elseif ($VMPowerstate -eq "PoweredOn") {
+		Shutdown-VMGuest $VM -Confirm:$false
+		get-vm $VM|get-networkadapter|set-networkadapter -type vmxnet3
+		Start-VM $VM
+	}
+}
--- powerCLI/Get-VMs.ps1
+++ powerCLI/Get-VMs.ps1
@@ -3,6 +3,6 @@ $FilePath = "c:\tools\Network_Interface.csv"
 get-networkadapter(get-vm) | where {$_.Type -eq "e1000"} | Select @{N="VM";E={$_.Parent.Name}},Type | export-Csv  $FilePath -NoTypeInformation
 write-host "All current VMs stored in " $FilePath
 write-host "Starting Get-OSversion.ps1 to filter out VMs other than Windows" 
-Invoke-Expression c:\scripts\test.ps1
+#Invoke-Expression c:\scripts\test.ps1
 
  
