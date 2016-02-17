1afe95d0a94a17c5443c78ddd48c69c06e4e9522
fredrik444 <b1934249@rmqkr.net>
2016-02-17 16:41:59 +0100

    Added logic to GetIpconfig.ps1
    Checks if adapter type is E1000 before saving IP adapter settings to
    IpSettings.csv. If adapter equals VMXNET3 then it will not save dapter
    settings.

--- Windows/GetIPConfig.ps1
+++ Windows/GetIPConfig.ps1
@@ -17,25 +17,28 @@ If ($(Test-Path $FilePath) -eq $FALSE) { #Stores NIC IP settings if config file 
 	#Fetching the active NIC IP settings
 	$index = (Get-WmiObject win32_networkadapterconfiguration -Filter 'ipenabled = "true"').index
 	$wmi = Get-WmiObject win32_networkadapterconfiguration -filter "Index = $index"
+	$wmiDescription = (Get-WmiObject win32_networkadapterconfiguration -Filter 'ipenabled = "true"').Description
 	
-	#Is DHCP enabled?
-	$DHCP = $wmi.DHCPEnabled
-	#Does folder already exist?
-	if ($(Test-Path $FolderPath) -eq $FALSE) { 
-		New-Item $FolderPath -type directory
+	if ($wmiDescription -like "Intel(R)*") {
+		#Is DHCP enabled?
+		$DHCP = $wmi.DHCPEnabled
+		#Does folder already exist?
+		if ($(Test-Path $FolderPath) -eq $FALSE) { 
+			New-Item $FolderPath -type directory
+			}
+		else {
+		#Don't do anything
 		}
-	else {
-	#Don't do anything
+		$IP = $wmi.IPAddress[0]
+		$MASK = $wmi.IPSubnet[0]
+		$GW = $wmi.DefaultIPGateway[0]
+		$MAC = $wmi.MACAddress
+		$DNS1 = $wmi.DNSServerSearchOrder[0]
+		$DNS2 = $wmi.DNSServerSearchOrder[1]
+		#Saves IP settings to file
+		"IP,MASK,GW,DNS1,DNS2,DHCP,MAC" -join ',' | Out-File -FilePath $FilePath -Width 200;
+		$IP,$MASK,$GW,$DNS1,$DNS2,$DHCP,$MAC -join ',' | Out-File -FilePath $FilePath -Append -Width 200;
 	}
-	$IP = $wmi.IPAddress[0]
-	$MASK = $wmi.IPSubnet[0]
-	$GW = $wmi.DefaultIPGateway[0]
-	$MAC = $wmi.MACAddress
-	$DNS1 = $wmi.DNSServerSearchOrder[0]
-	$DNS2 = $wmi.DNSServerSearchOrder[1]
-	#Saves IP settings to file
-	"IP,MASK,GW,DNS1,DNS2,DHCP,MAC" -join ',' | Out-File -FilePath $FilePath -Width 200;
-	$IP,$MASK,$GW,$DNS1,$DNS2,$DHCP,$MAC -join ',' | Out-File -FilePath $FilePath -Append -Width 200;
 }
 else {
 	#write-host "file exist, skipping"
