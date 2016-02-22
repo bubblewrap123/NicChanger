21d03285d79b1d5e135819cdd543070e2edf48c7
fredrik444 <b1934249@rmqkr.net>
2016-02-22 16:45:20 +0100

    Added disclaimer when running Change-NIC.ps1

--- powerCLI/Change-NIC.ps1
+++ powerCLI/Change-NIC.ps1
@@ -1,5 +1,5 @@
 #    /*******************************************************
-#    * Change-NIC.ps1 -- Uses data from Get-VMOSPS.ps1      *
+#    * Change-NIC.ps1 -- Uses data from Get-OSandPowerstate *
 #    *                                                      *
 #    * Author:  Fredrik, Treimo                             *
 #    *                                                      *
@@ -11,18 +11,39 @@
 
 $FilePath = "c:\tools\Network_Interface_Sorted.csv"
 
+Function pause ($message)
+{
+    # Check if running Powershell ISE
+    if ($psISE)
+    {
+        Add-Type -AssemblyName System.Windows.Forms
+        [System.Windows.Forms.MessageBox]::Show("$message")
+    }
+    else
+    {
+        Write-Host "$message" -ForegroundColor Yellow
+        $x = $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    }
+}
+
+write-host "DISCLAIMER: USE THIS SCRIPT AT YOUR OWN RISK!"
+write-host "THE AUTHOR TAKES NO RESPONSIBILITY FOR THE RESULTS OF THIS SCRIPT."
+write-host "Disclaimer aside, this worked for the author, for what that's worth."
+pause "Press any key to continue"
+
+
 import-CSV $FilePath | ForEach-Object {
 	$VM = $_.VM
 	$VMPowerstate = $_.Powerstate
 	
-	if ($VMPowerstate -eq "PoweredOff") { #Må tenke litt på hva vi gjør her. Trenger vi å skru den på først, for at den skal lagre IP settings via scheduled task?
+	if ($VMPowerstate -eq "PoweredOff") { #We don't want to change network adapter on a VMs which has not run the GetIPConfig.ps1.
 		#get-vm $VM|get-networkadapter|set-networkadapter -type vmxnet3 -Confirm:$false
 		#write-host "Nic changed on:" $VM
 		#Start-VM $VM #-RunAsync
 		#write-host $VM "started"
 	}
 	elseif ($VMPowerstate -eq "PoweredOn") { 
-		Shutdown-VMGuest $VM -Confirm:$false #We need to check if the guest OS is shut down before proceeding.. The next lines will fail otherwize.
+		Shutdown-VMGuest $VM -Confirm:$false #We need to check if the guest OS is shut down before proceeding..
 		while (((get-vm $VM).powerstate) -eq "PoweredOn") {
 			 start-sleep 10
 		}
