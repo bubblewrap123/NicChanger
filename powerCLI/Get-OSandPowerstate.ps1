9b31d4ad5ce177b060eeb570bc045dfcc89267f8
bubblewrap123 <fredrik44444@gmail.com>
2016-03-07 13:21:32 +0100

    fixed some typs..

--- powerCLI/Get-OSandPowerstate.ps1
+++ powerCLI/Get-OSandPowerstate.ps1
@@ -1,7 +1,7 @@
 #    /*******************************************************
 #    * Get-VMOSPS.ps1 -- Started from Get-VMs.ps1           *
 #    *                                                      *
-#    * Author:  Fredrik,                                    *
+#    * Author:  Fredrik, T                                  *
 #    *                                                      *
 #    * Purpose:  Sorting VMs into VM name & powerstate      *
 #    *                                                      *
